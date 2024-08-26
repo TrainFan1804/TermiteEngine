54e39d5bde8737d8382b6f908fed38d75d6f3beb
o.le <oleschulz123@gmail.com>
2024-08-26 18:27:37 +0200

    Start working on the save system

--- termite/engine/core/subsystems/save/SaveSystem.java
+++ termite/engine/core/subsystems/save/SaveSystem.java
@@ -1,6 +1,13 @@
 package engine.core.subsystems.save;
 
+import engine.core.ApplicationResources;
 import engine.core.subsystems.EngineSubsystem;
+import engine.instance.Instance;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author                              o.le
@@ -11,6 +18,24 @@ class SaveSystem implements EngineSubsystem {
 
     @Override
     public void execute() {
+
+		Instance i = ApplicationResources.GAME.getCurrentInstance();
+
+		File file = new File("savefile.txt");
+		FileWriter writer = null;
+		try {
+
+			writer = new FileWriter(file);
+			writer.write("Test");
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		try {
+			writer.close();
+		} catch (IOException ex) {
+			Logger.getLogger(SaveSystem.class.getName()).log(Level.SEVERE, null, ex);
+		}
 
         System.out.println("Save game..");
     }
