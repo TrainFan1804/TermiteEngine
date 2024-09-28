abcf66ad1b8a1c98aa9abf700294dc37b950e987
o.le <oleschulz123@gmail.com>
2024-09-28 08:17:44 +0200

    Remove module system

--- src/module-info.java
+++
@@ -1,7 +0,0 @@
-module Termiteengine {
-	requires com.fasterxml.jackson.databind;
-	requires com.fasterxml.jackson.annotation;
-	requires com.fasterxml.jackson.core;
-
-	exports de.o.le.termiteengine.api;
-}
