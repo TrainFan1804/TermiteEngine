a40e5649f16ec95c20fa4f8508cd449159f39f44
Ole Schulz <ole.schulz@ostfalia.de>
2025-12-17 13:51:59 +0100

    Changing RoomInfo to public

--- engine/src/main/java/de/o/le/termite/data/Room.java
+++ engine/src/main/java/de/o/le/termite/data/Room.java
@@ -4,19 +4,27 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
  * @author                              o.le
- * @version                             1.0
+ * @version                             1.1
  * @since                               25.12.3
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Room {
 
-    static class RoomInfo {
+    public static class RoomInfo {
         String name;
         String description;
         String image;
+
+        public String getName() { return name; }
+
+        public String getDescription() { return description; }
+
+        public String getImage() { return image; }
     }
 
     private RoomInfo info;
+
+    public RoomInfo getInfo() { return this.info; }
 
     public String getName() {
         return info.name;
